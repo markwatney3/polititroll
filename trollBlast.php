41259da0d23a6b82ee9486bacd4c511ea022f6a8
Devon Papandrew <devon.papandrew@gmail.com>
2015-08-15 12:21:19 -0400

    commit2

--- trollBlast.php
+++ trollBlast.php
@@ -2,50 +2,53 @@
 
 require '/home/ubuntu/vendor/autoload.php';
 
-$senderName = $_POST["sender"];//"Devon";
-//$candidate = "Trump";
-$phoneNumber = $_POST["phone"];//"7276418026";
+$senderName = $_POST["sender"];
+$candidate = $_POST["candidate"];
+$phoneNumber = $_POST["phone"];
 $carrier = $_POST["carrier"];
-
 
 $handleArray = array(
 	
 	"Verizon" => "@vzwpix.com",
 	"ATT" => "@mms.att.net",
-	"Sprint" => "@messaging.sprintpcs.com",
+	"Sprint" => "@pm.sprint.com",
 	"TMobile" => "@tmomail.net", 
 	
 );
 
 $targetEmail = $phoneNumber.$handleArray[$carrier];
 
-
 $memesList = array("0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "13.jpg", "14.jpg");
-
 //compiles a list of the filepaths of the memes that match the candidate
+shuffle($memesList);
 
-$memesIndex = range(1, count($memesList));
-shuffle($memesIndex);
-
+try{
 	$email = new PHPMailer();
 	$email->SetFrom($senderName."@polititroll.me");
-//	$email->AddReplyTo($senderName."@mail.polititroll.me", $senderName);
-//	$email->FromName  = 'Polititroll';
-	$email->Subject   = "";
+	$email->AddReplyTo($senderName."@polititroll.me", $senderName);
+	$email->FromName  = $senderName;
+	$email->Subject   = "Polititroll";
 	$email->AddAddress( $targetEmail );
 	$email->Body      = "You are being #Polititrolled by your friend ".$senderName."! Polititroll them back at polititroll.me ! Or, reply STOP if you do not wish to recieve trolls from ".$senderName." or your other friends after this blast.";
 
-
-for($i=0; $i<2; $i++){
+for($i=0; $i<5; $i++){
 	
-	$file_to_attach = 'images/'.$memesList[$memesIndex[$i]];
+	$file_to_attach = 'images/'.$memesList[$i];
 	$email->AddAttachment($file_to_attach);
 	
 }
 
 	$email->Send();
 
-echo "Success! We've blasted your friend with a #Polititroll!";
+echo "Success! We've blasted your friend with a #Polititroll! \n";
 
+echo $phoneNumber.",".$senderName.",".candidate.",".$carrier.",".$targetEmail;
+
+
+} catch (phpmailerException $e) {
+  echo $e->errorMessage();
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
 ?> 
 
