b3ffa3d0f5e8634e0387d28d18fcb0a5fb73d432
Devon Papandrew <devon.papandrew@gmail.com>
2015-08-15 23:04:58 -0400

    1

--- trollBlast.php
+++ trollBlast.php
@@ -24,8 +24,6 @@ $response = json_decode($response, true);
 
 $targetEmail = $response["response"]["results"][0]["mms_address"];
 
-echo $targetEmail;
-
 $memesList = array("0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "13.jpg", "14.jpg");
 //compiles a list of the filepaths of the memes that match the candidate
 shuffle($memesList);
