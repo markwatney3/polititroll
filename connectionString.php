565d7ad8fb224bb352edeb21b6cc2ec66449ac85
Devon Papandrew <devon.papandrew@gmail.com>
2015-08-18 16:22:28 -0400

    1

---
+++ connectionString.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "polititroll.cqre5u0pvjqm.us-east-1.rds.amazonaws.com";
+$username = "polititroll";
+$password = "rohanisgay";
+$dbName = "polititroll";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+ echo "Connected successfully";
+?>
--- index.php
+++ index.php
@@ -18,14 +18,33 @@ Your Name:<br>
 <input type="text" name="sender">
 <br>
 Candidate: (this isnt turned on yet)<br>
- <select name="candidate">
-<option value="trump">Donald Trump</option>
-<option value="clinton">Hillary Clinton</option>
-<option value="paul">Ron Paul</option>
-<option value="bush">Jeb Bush</option>
+	
+<!--
+	
+	<select name="candidate">
+-->
+<?php 
+//echo "badb";
+//require 'connectionString.php';
+//echo "something";
+//$sql = mysql_query("SELECT candidate FROM Candidate");
+//$result = $conn->query($sql);
+//
+//if ($result->num_rows > 0) {
+//    // output data of each row
+//    while($row = $result->fetch_assoc()) {
+////        echo "<option value='".$row['candidate']."'>" . $row['candidate'] . "</option>";
+//		echo $row['candidate'];
+//		echo "lalala";
+//    }
+//}
+?>
+<!--
+
+<option vale="test">test</option>
+	 
 </select>
-
-
+-->
 
 <br><br>
 	<b>Troll</b> your friends if they support the candidate you selected, and need to learn a thing or two!
