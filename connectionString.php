86e97f31d077c20e2a79e3b34eb7bb845f8d7bfe
Devon Papandrew <devon.papandrew@gmail.com>
2015-08-18 16:35:27 -0400

    fuck 2

--- connectionString.php
+++ connectionString.php
@@ -11,5 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbName);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
- echo "Connected successfully";
+// echo "Connected successfully";
 ?>
--- trollBlast.php
+++ trollBlast.php
@@ -10,6 +10,7 @@
 
 
 require '/home/ubuntu/vendor/autoload.php';
+require 'connectionString.php';
 
 $senderName = $_POST["sender"];
 $candidate = $_POST["candidate"];
@@ -23,20 +24,6 @@ else if ($_POST["applaud"] == "Applaud"){
 	$troll = 0;
 }
 
-
-$servername = "polititroll.cqre5u0pvjqm.us-east-1.rds.amazonaws.com";
-$username = "polititroll";
-$password = "rohanisgay";
-$dbName = "polititroll";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbName);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-// echo "Connected successfully";
 
 $sql= "SELECT handle, carrier FROM Carrier WHERE phone_number = ".$phoneNumber;
 $result = $conn->query($sql);
